2ff0c3523f08d4c069a9c07a84b3219744be16c8
Anatolii_Gorstka <Logan46@yandex.ru>
2022-10-23 14:07:16 +0300

    add tests for tree gendiff'

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 import parsers from '../src/parsers.js';
 import format from '../src/formatters/index.js';
+import stylish from '../src/formatters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,20 +12,29 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing stylish for plain files json', () => {
+test('testing result for tree files json', () => {
   const expected = readFile('testFile.txt');
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const actual = genDiff(getFixturePath('treefile1.json'), getFixturePath('treefile2.json'), 'stylish');
   expect(actual).toBe(expected);
 });
-test('testing stylish for plain files yml', () => {
+test('testing result for tree files yml', () => {
   const expected = readFile('testFile.txt');
-  const actual = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  const actual = genDiff(getFixturePath('treefile1.yml'), getFixturePath('treefile2.yml'));
   expect(actual).toBe(expected);
 });
 test('testing stylish for plain files throw parsers', () => {
   expect(() => parsers('mjs')).toThrow('Формат не поддерживается: mjs');
 });
-test('testing stylish for plain files throw formatters', () => {
+test('testing format for throw formatters', () => {
   const a = [{ type: '1' }];
-  expect(() => format(a)).toThrow('Этого типа не существует: 1');
+  expect(() => format(a, 2)).toThrow('Формат не поддерживается: 2');
+});
+test('testing format for json', () => {
+  const a = { x: 5 };
+  const b = format(a, 'json');
+  expect(b).toBe('{"x":5}');
+});
+test('testing stylish for throw wrong type', () => {
+  const a = [{ type: 'sam' }];
+  expect(() => stylish(a)).toThrow(new Error('Этого типа не существует: sam'));
 });
